75dc5afc97f4f67aff9076cbd1283b66e686762c
anbugz <andreybugz@gmail.com>
2026-05-17 16:24:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ else:
     _default_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot.db")
 DB_PATH = os.getenv("DB_PATH", _default_db)
 
-VERSION = "rev11 | 2026-05-17 | АБ"
+VERSION = "rev12 | 2026-05-17 | АБ"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
