c38ce4f18b4abd57eedfdc1858c62316d6b4c9ad
anbugz <andreybugz@gmail.com>
2026-05-18 10:04:13 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ else:
     _default_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot.db")
 DB_PATH = os.getenv("DB_PATH", _default_db)
 
-VERSION = "rev14 | 2026-05-18 | TOPIC_DETECT"
+VERSION = "rev15 | 2026-05-18 | MATERIAL_LEMMA"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger(__name__)
